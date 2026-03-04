d4c8e4cf02dcaf297bee3de3224bfd7d8ed6b82c
PARK JU YEONG <96644508+parkjuyeong0312@users.noreply.github.com>
2026-03-04 10:35:50 +0900

    [Gold IV] Title: Σ, Time: 196 ms, Memory: 20100 KB -BaekjoonHub

---
+++ 백준/Gold/13172. Σ/Σ.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long M;
+    static long MOD = 1000000007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        M = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        long ans=0;
+        //입력
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int N=Integer.parseInt(st.nextToken());
+            int S=Integer.parseInt(st.nextToken());
+
+            ans += S*func(N, MOD-2)%MOD;
+            ans%=MOD;
+//            result = func(N,MOD-2);
+        }
+
+        System.out.println(ans);
+
+    }
+
+    static long func(long N,long degree){
+        if(degree==0){
+            return 1;
+        }
+
+        long half = func(N,degree/2);
+
+        long result = half*half%MOD;
+        if(degree%2!=0) result*=N%MOD;
+
+        return result%MOD;
+    }
+}
